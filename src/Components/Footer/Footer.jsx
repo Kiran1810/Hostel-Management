adf7318136d6788717e77fd0384be066c2601c41
karanpal97 <karangill1810@gmail.com>
2024-01-11 20:33:36 +0530

    improved the footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -13,7 +13,7 @@ const LINKS = [
   },
   {
     title: "CONTACT",
-    items: ["📍 STUDENT ACTIVITY CENTER, NIT", "HIMACHAL PRADESH 177005", "📧 CONTACT@ECELL.IN"],
+    items: ["📍 STUDENT ACTIVITY CENTER, NIT", "HIMACHAL PRADESH 177005", "📧 CONTACT"],
   },
 ];
 
@@ -22,11 +22,11 @@ const currentYear = new Date().getFullYear();
 const Footer = () => {
   return (
     <footer className="relative w-full text-gray-400">
-      <div className="mx-auto w-full px-8 border-t-2 border-blue-gray-50 pt-4">
+      <div className="mx-auto w-full lg:px-8 px-2 border-t-2 border-blue-gray-50 pt-4">
         <div className="grid grid-cols-1 justify-between gap-4 md:grid-cols-2">
           <Typography variant="h5" className="lg:mt-6 lg:pl-20 ml-2 text-gray-100 items-center flex justify-start">
-            <img src={Logo} alt="ESummit @ NITH" className="p-8 lg:w-48 lg:h-48 w-32 h-32 border-white border-r-2" />
-            <h1 className="w-48 text-xl ml-4 font-serif">THE<br />ENTREPRENEURSHIP CELL <br /><br />NIT HAMIRPUR</h1>
+            <img src={Logo} alt="ESummit @ NITH" className="lg:p-8 p-3 lg:w-48 lg:h-48 w-32 h-32 border-white border-r-2" />
+            <h1 className="w-48 text-xl ml-4 font-serif">THE<br />ENTREPRENEUR CELL <br /><br />NIT HAMIRPUR</h1>
           </Typography>
           <div className="grid grid-cols-3 text-md font-kelly mt-4 justify-between gap-4">
             {LINKS.map(({ title, items }) => (
