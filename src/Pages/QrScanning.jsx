b56034765ab13477c75111e8e023ef99a3a7d6a1
karanpal97 <karangill1810@gmail.com>
2024-01-14 15:14:32 +0530

    implemented the qr scanner in the project

--- src/Pages/QrScanning.jsx
+++ src/Pages/QrScanning.jsx
@@ -1,14 +1,18 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import QrScanner from 'react-qr-scanner';
 
 const QRScannerComponent = () => {
   const [result, setResult] = useState('');
 
   const handleScan = (data) => {
-    if (data) {
-      setResult(data);
-    }
+    setResult(data);
   };
+
+  useEffect(() => {
+    if (result && typeof result === 'object') {
+      console.log('Scanned result:', result);
+    }
+  }, [result]);
 
   const handleError = (err) => {
     console.error(err);
@@ -20,9 +24,9 @@ const QRScannerComponent = () => {
         onScan={handleScan}
         onError={handleError}
         style={{ width: '100%' }}
-        facingMode="environment" // Specify "environment" for the back camera
+        facingMode="environment"
       />
-      {result && <p>Scanned result: {result}</p>}
+      {result && <p>Scanned result: {result.text}</p>}
     </div>
   );
 };
